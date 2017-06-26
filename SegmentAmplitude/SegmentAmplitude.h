585ead0c960142030ec6dd5fedc41e5d54fb04cb
Giorgio Paoloni <giorgio@Firenze-Pro.local>
2017-06-26 19:42:16 -0300

    Updating Umbrella

--- SegmentAmplitude/SegmentAmplitude.h
+++ SegmentAmplitude/SegmentAmplitude.h
@@ -15,5 +15,5 @@ FOUNDATION_EXPORT double SegmentAmplitudeVersionNumber;
 FOUNDATION_EXPORT const unsigned char SegmentAmplitudeVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <SegmentAmplitude/PublicHeader.h>
-
-
+#import <SegmentAmplitude/SEGAmplitudeIntegration.h>
+#import <SegmentAmplitude/SEGAmplitudeIntegrationFactory.h>
